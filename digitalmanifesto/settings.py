bd8607e9a1c55c76dc0f66cf6dfdfa9360dfa975
gwhigs <gwhigs@gmail.com>
2015-05-13 21:28:36 -0400

    Removed admin logging emails until Sendgrid is setup on Heroku

--- digitalmanifesto/settings.py
+++ digitalmanifesto/settings.py
@@ -38,9 +38,9 @@ ALLOWED_HOSTS = [
     '.digitalmanifesto.net',
 ]
 
-ADMINS = [
-    ('Graham Higgins', 'gwhigs@gmail.com'),
-]
+# ADMINS = [
+#     ('Graham Higgins', 'gwhigs@gmail.com'),
+# ]
 
 # CREDIT TO https://github.com/eknuth/django-heroku-s3-bootstrap-demo/blob/master/om/settings.py
 # A sample logging configuration. The only tangible logging
@@ -48,23 +48,23 @@ ADMINS = [
 # the site admins on every HTTP 500 error.
 # See http://docs.djangoproject.com/en/dev/topics/logging for
 # more details on how to customize your logging configuration.
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'mail_admins': {
-            'level': 'ERROR',
-            'class': 'django.utils.log.AdminEmailHandler'
-        }
-    },
-    'loggers': {
-        'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
-            'propagate': True,
-        },
-    }
-}
+# LOGGING = {
+#     'version': 1,
+#     'disable_existing_loggers': False,
+#     'handlers': {
+#         'mail_admins': {
+#             'level': 'ERROR',
+#             'class': 'django.utils.log.AdminEmailHandler'
+#         }
+#     },
+#     'loggers': {
+#         'django.request': {
+#             'handlers': ['mail_admins'],
+#             'level': 'ERROR',
+#             'propagate': True,
+#         },
+#     }
+# }
 
 # Application definition
 

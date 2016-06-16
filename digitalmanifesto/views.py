c025092d20dae5eb5710bae75d68e28d366b994a
gwhigs <gwhigs@gmail.com>
2016-06-16 18:36:31 -0400

    Removes unnecessary template views; removes lingering grappelli code

--- digitalmanifesto/views.py
+++ digitalmanifesto/views.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import, unicode_literals
 from django.http import HttpResponse
 from django.views.generic import TemplateView
 
-from manifestos.models import Manifesto, Collection
+from manifestos.models import Collection
 
 
 LETSENCRYPT_SECRET = 'RoqK1ZHN6384upsmMKbrJuxqaGNKcmJc5JApOy8qi8Y'
@@ -21,23 +21,3 @@ class IndexView(TemplateView):
         context = super(IndexView, self).get_context_data(**kwargs)
         context['collection'] = Collection.objects.get(featured=True)
         return context
-
-
-class AboutTemplateView(TemplateView):
-    template_name = 'about.html'
-
-
-class ContactTemplateView(TemplateView):
-    template_name = 'contact.html'
-
-
-class NewsTemplateView(TemplateView):
-    template_name = 'news.html'
-
-
-class ProjectsTemplateView(TemplateView):
-    template_name = 'projects_we_like.html'
-
-
-class ResourcesTemplateView(TemplateView):
-    template_name = 'resources.html'

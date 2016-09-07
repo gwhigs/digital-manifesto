5ab136a9ee0bf2ebbf09ecb44ed0136253889580
gwhigs <gwhigs@gmail.com>
2016-09-07 14:48:01 -0400

    Remove unused import

--- annotations/api_urls.py
+++ annotations/api_urls.py
@@ -4,7 +4,7 @@ All urls for the Annotations API go here.
 
 from __future__ import absolute_import, unicode_literals
 
-from django.conf.urls import url, include
+from django.conf.urls import url
 
 from . import views
 
@@ -18,9 +18,8 @@ router.register(r'annotations', views.AnnotationViewSet)
 
 urlpatterns = router.urls
 
-# Optional 3rd party generated documentation
-
 urlpatterns += (
     url(r'^$', views.AnnotationIndexAPI.as_view(), name='index'),
+    # Optional 3rd party generated documentation
     # url(r'^docs/', include('rest_framework_swagger.urls')),
 )

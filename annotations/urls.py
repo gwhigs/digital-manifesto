3e9386e09469c8f787f1f4efb070919b5fb961b3
gwhigs <gwhigs@gmail.com>
2016-09-13 10:34:14 -0400

    Remove unnecessary comment

--- annotations/urls.py
+++ annotations/urls.py
@@ -5,6 +5,5 @@ from django.conf.urls import url, include
 from . import views, api_urls
 
 urlpatterns = [
-    # url(r'^$',  ManifestoListView.as_view(), name='index'),
     url(r'^api/', include(api_urls, namespace='api'))
 ]

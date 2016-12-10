c40a07e4ba1bfefd977bc9eea71abe5fcaf97370
Graham Higgins <gwhiggins@gannett.com>
2016-12-10 11:12:38 -0500

    Use custom exception in place of NotImplemented

--- manifestos/twitter.py
+++ manifestos/twitter.py
@@ -9,6 +9,10 @@ TWITTER_ACCESS_KEY = settings.TWITTER_ACCESS_KEY
 TWITTER_ACCESS_SECRET = settings.TWITTER_ACCESS_SECRET
 
 
+class TwitterBotException(Exception):
+    pass
+
+
 class TwitterBot(object):
     """
     Creates tweets for the Digital Manifest Twitter Bot.
@@ -23,8 +27,13 @@ class TwitterBot(object):
         return tweepy.API(auth)
 
     def tweet(self, text):
+        # Make sure we have legitimate text to tweet
         if not isinstance(text, str):
-            raise NotImplemented('Can only tweet strings.')
+            raise TwitterBotException('Can only tweet strings.')
+        text = text.strip()
+        if not text:
+            raise TwitterBotException('Text has no content.')
+
         # Escape SMS commands
         pattern = re.compile(
             r'^(ON|OFF|FOLLOW|F|UNFOLLOW|LEAVE|L|STOP|QUIT|END|CANCEL|'
@@ -33,6 +42,9 @@ class TwitterBot(object):
             r'BLOCK|BLK|REPORT|REP)(\W)(.*)', re.I)
         text = re.sub(pattern, '\\1\u200B\\2\\3', text)
 
+        # Truncate to 140 characters
         text = text[:140]
+
+        # Tweet
         api = self.get_api()
         api.update_status(status=text)
